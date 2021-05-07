82438f0076317cfd437d33cc629a9a31b524a73e
Maciej Obarski <mobarski@gmail.com>
2021-05-07 23:24:33 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # thorvald
 Similarity calculation engine for item-based collaborative filtering on unary data.
+
+**WARNING** - this is pre-alpha version.
+
+# Intro
+
+TODO
+
+## Example input file
+
+```
+item	users
+i1	u1,u2,u3,u4,u5,u6
+i2	u1,u3,u5,u7,u9
+i3	u2,u4,u6,u8
+i4	u1,u2,u3,u7,u8
+```
+
+## Example output file
+
+```
+aname	bname	c
+i1	i1	6
+i1	i2	3
+i1	i3	3
+i1	i4	3
+i2	i2	5
+i2	i4	3
+i3	i3	4
+i3	i4	2
+i4	i4	5
+```
+
+## Invocation
+
+```./thorvald -i input.tsv -o output-%d.tsv -wrk 2```
+
+## Performance
+
+TODO
+
+## Features
+
+TODO
+
+- KMV sketch based acceleration
+- parallel processing
+
+# CLI options
+
+|       option | info |
+| -----------: | ---- | 
+|        **i** | input path |
+|        **o** | output path, %d will be replaced with partition number |
+|      **fmt** | output format, (default: aname,bname,c) |
+|      **buf** | line buffer capacity in MB (default: 10) |
+|  **colitem** | column number of item name (1-based) (default: 1) |
+| **colusers** | column number of users names (1-based) (default: 2) |
+|      **wrk** | number of workers (default: 1) |
+|     **cmin** | minimum number of common users to show in output (default: 1) |
+
+## Output format
+
+|        option | info |
+| ------------: | ---- |
+|         **a** | number of users of item A |
+|        **ai** | index of item A |
+|     **aname** | name of item A |
+|     **anpmi** | absolute NPMI |
+|         **b** | number of users of item B |
+|        **bi** | index of item B |
+|     **bname** | name of the |
+|         **c** | number of users common to item A and item B |
+|        **ci** | result index in 1d array -> ai*num_items + bi |
+|       **cos** | cosine similarity |
+|      **craw** | raw number of elements in intersection of sketch A and B |
+|      **dice** | Soersen-Dice index |
+|   **jaccard** | Jaccard index |
+|      **lift** | lift |
+|      **npmi** | NPMI - Normalized Pointwise Mutual Information |
+|   **overlap** | overlap |
+| **partition** | partition/worker ID  |
+|       **pmi** | PMI - Pointwise Mutual Information |
+|  **tanimoto** | Tanimoto index |
+
+# Planed features
+
+- sketch input
+- sketch output
+- sketch delta update
+- distributed processing support
+- better output limiter
+- asymetrical metrics
+- non-triangular output
+- item,context,users input format
+- item-item combinations reduction via item features
+
+[//]: # (online .md editor: https://markdown-editor.github.io/)
---
+++ src/thorvald.go
@@ -0,0 +1,355 @@
+// v4 -> parametry CLI
+// v4 -> output format
+// v4 -> item zamiast movie
+
+package main
+  
+import (
+    "bufio"
+    "fmt"
+    //"log"
+    "os"
+	"time"
+	"strings"
+	"hash/crc32"
+	"sort"
+	"sync"
+	"math"
+	"flag"
+	//"runtime"
+	"sync/atomic" // atomic.AddUint32(v, 1) <- (v *uint32)
+)
+
+func min(x, y int) int {
+    if x < y {
+        return x
+    }
+    return y
+}
+
+func max_u32(x, y uint32) uint32 {
+    if x < y {
+        return y
+    }
+    return x
+}
+
+// --- PROGRESS ---
+
+type bar struct {
+	total uint64
+	label string
+	unit  string
+	done  uint64
+	t0    time.Time
+}
+
+func Bar(total int, label string, unit string) bar {
+	return bar{uint64(total), label, unit, 0, time.Now()}
+}
+
+func (b *bar) Add(x int) {
+	atomic.AddUint64(&b.done,uint64(x))
+	elapsed := time.Since(b.t0)
+	rate := float64(b.done) / elapsed.Seconds()
+	if b.total>0 {
+		done_pct := float64(b.done) / float64(b.total) * 100
+		// width := 40
+		// bar_done_cnt := int(done_pct / (100. / float64(width)))
+		// bar_todo_cnt := width - bar_done_cnt
+		// bar_done_str := strings.Repeat("=",bar_done_cnt)
+		// bar_todo_str := strings.Repeat("-",bar_todo_cnt)
+		fmt.Printf("\r%s: %d / %d %s (%.f%%) -> %.1fs (%.1f %s/s)", b.label, b.done, b.total, b.unit, done_pct, elapsed.Seconds(), rate, b.unit)
+	} else {
+		fmt.Printf("\r%s: %d %s -> %.1fs (%.1f %s/s)", b.label, b.done, b.unit, elapsed.Seconds(), rate, b.unit)
+	}
+	// TODO: remaining
+	// TODO: rate
+	// TODO: bar
+}
+
+func (b *bar) Close() {
+	fmt.Printf("\n")
+}
+
+// --- SKETCH UTILS ---
+
+func estimate_count(sketch_len int, v_max uint32) int {
+	if sketch_len>0 {
+		return int(math.Round(float64(sketch_len)/float64(v_max) * float64(4294967295)))
+	} else {
+		return 0
+	}
+}
+
+func estimate_intersection(sketch_len int, sketch_cap int, a_cnt int, b_cnt int, v_max uint32) int {
+	if a_cnt<sketch_cap && b_cnt<sketch_cap {
+		return sketch_len
+	} else {
+		c := estimate_count(sketch_len, v_max)
+		return min(c,min(a_cnt,b_cnt))
+	}
+}
+
+func press_enter(msg string) {
+	fmt.Println(msg)
+	var x string; fmt.Scanf("%s",&x)
+}
+
+
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type Cfg struct {
+	input_path  string
+	output_path string
+	buf_cap     int
+	sketch_cap  int
+	item_col    int
+	users_col   int
+	output_fmt  string
+	workers     int
+	top         int // NOT USED
+	c_min       int
+}
+
+func core(cfg *Cfg) {
+	buf_cap     := cfg.buf_cap * 1024*1024
+	sketch_cap  := cfg.sketch_cap
+	input_path  := "../data/v1-big.tsv" // TODO
+	output_path := "../output/big-cap-1000.p%d.tsv" // TODO
+	item_col    := cfg.item_col-1
+	users_col   := cfg.users_col-1
+	output_fmt  := []string{"aname","bname","c"} // TODO
+	workers     := cfg.workers // TODO jezeli=0 to tyle ile cpu
+	c_min       := cfg.c_min
+	//top         := cfg.top
+	
+	// --- SET / KMV SKETCH CONSTRUCTION --------------------------------------
+	
+	crcTable := crc32.MakeTable(crc32.Castagnoli)
+	file, err := os.Open(input_path)
+	check(err)
+	
+	buf := make([]byte, buf_cap)
+	scanner := bufio.NewScanner(file)
+	scanner.Buffer(buf, buf_cap)
+	scanner.Split(bufio.ScanLines)
+	scanner.Scan() // header
+	
+	users_by_item := make(map[string]map[uint32]bool)
+	range_by_item := make(map[string]int)
+	all_users := make(map[uint32]bool)
+	
+	fo,err := os.Create("sketch.estimation.tsv") // TODO: co z tym plikiem ???
+	check(err)
+	w := bufio.NewWriter(fo)
+	
+	bar := Bar(0,"LOAD","items")
+	for scanner.Scan() {
+		text := scanner.Text()
+		rec := strings.Split(text, "\t")
+		//fmt.Printf("len rec[0]=%d rec[1]=%d \n",len(rec[0]),len(rec[1]))
+		item := rec[item_col]
+		users := strings.Split(rec[users_col], ",") // TODO: check error
+		
+		// SKETCH
+		users_hash := make([]int, 0, len(users)) // TODO: rename users_hash
+		for _,user := range users {
+			hash := int(crc32.Checksum([]byte(user), crcTable))
+			users_hash = append(users_hash, hash)
+			all_users[uint32(hash)] = true // not a sketch part -> for metrics
+		}
+		var sketch []int // TODO: rename sketch
+		if sketch_cap>0 {
+			sort.Ints(users_hash)
+			sketch_len := min(len(users_hash),sketch_cap)
+			sketch = users_hash[:sketch_len]
+		} else {
+			sketch = users_hash
+		}
+		
+		// SET
+		users_set := make(map[uint32]bool)
+		for _,hash := range sketch {
+			users_set[uint32(hash)] = true
+		}
+		users_by_item[item] = users_set
+		range_by_item[item] = len(users_hash)
+		
+		bar.Add(1)
+	}
+	w.Flush()
+	fo.Close()
+	file.Close()
+	bar.Close()
+
+	//fmt.Printf("items[1] users cnt %d\n",len(users_by_item["f1"])) // XXX 
+	//fmt.Printf("items[2] users cnt %d\n",len(users_by_item["f2"])) // XXX
+	
+	
+	// --- INTERSECTION -------------------------------------------------------
+	
+	//items_cnt := len(users_by_item)
+	items_cnt := 100 // XXX
+	items := make([]string, 0, items_cnt)
+	for name := range users_by_item {
+		items = append(items, name)
+	}
+	sort.Strings(items)
+	
+	all_users_cnt := len(all_users)
+	bar = Bar(items_cnt,"CALC","items")
+	var wg sync.WaitGroup
+	f := func(i0,ii int) {
+		filename := fmt.Sprintf(output_path,i0)
+		fo,err := os.Create(filename)
+		check(err)
+		w := bufio.NewWriter(fo)
+		//
+		for i:=i0; i<items_cnt; i+=ii {
+			mi := items[i]
+			mi_cnt := range_by_item[mi] // exact value - not from sketch
+			for j:=i; j<items_cnt; j++ {
+				
+				// --- INTERSECTION ---
+				common_cnt := 0
+				mj := items[j]
+				mj_cnt := range_by_item[mj] // exact value - not from sketch
+				v_max := uint32(0)
+				if i==j {
+					common_cnt = mi_cnt
+				} else {
+					// set intersection - iter on smaller
+					var a map[uint32]bool
+					var b map[uint32]bool
+					if mi_cnt < mj_cnt {
+						a = users_by_item[mi]
+						b = users_by_item[mj]
+					} else {
+						b = users_by_item[mi]
+						a = users_by_item[mj]
+					}
+					for u := range a {
+						_,ok := b[u]
+						if ok {
+							common_cnt += 1
+							v_max = max_u32(u, v_max)
+						}
+					}
+				}
+
+				// SKETCH intersection estimation
+				common_cnt_raw := common_cnt
+				if sketch_cap>0 && i!=j {
+					common_cnt = estimate_intersection(common_cnt, sketch_cap, mi_cnt, mj_cnt, v_max)
+				}
+								
+				// --- METRICS ---
+				a        := mi_cnt
+				b        := mj_cnt
+				c        := common_cnt
+				cos      := float64(c) / math.Sqrt(float64(a*b))
+				jaccard  := float64(c) / float64(a+b-c)
+				dice     := float64(2*c) / float64(a+b)
+				overlap  := float64(c) / float64(min(a,b))
+				lift     := float64(c) / float64(a*b) * float64(all_users_cnt)
+				pmi      := math.Log(lift)
+				npmi     := pmi / -math.Log(float64(c) / float64(all_users_cnt))
+				anpmi    := math.Abs(npmi)
+				tanimoto := float64(c) / (float64(a*a) + float64(b*b) - float64(c))
+
+				// --- OUTPUT ---
+				if c < c_min {
+					continue
+				}
+				// TODO: limitowanie outputu na podstawie jakiejs metryki -> overlap? abs(npmi)?
+				
+				// TODO: col -> inty zamiast stringow, przekodowanie na poczatku programu
+				for k,col := range output_fmt {
+					switch col {
+						case "aname"     : fmt.Fprintf(w, "%s", mi)
+						case "bname"     : fmt.Fprintf(w, "%s", mj)
+						case "partition" : fmt.Fprintf(w, "%d", i0)
+						case "ai"        : fmt.Fprintf(w, "%d", i)
+						case "bi"        : fmt.Fprintf(w, "%d", j)
+						case "ci"        : fmt.Fprintf(w, "%d", i*items_cnt+j)
+						case "a"         : fmt.Fprintf(w, "%d", a)
+						case "b"         : fmt.Fprintf(w, "%d", b)
+						case "c"         : fmt.Fprintf(w, "%d", c)
+						case "craw"      : fmt.Fprintf(w, "%d", common_cnt_raw)
+						case "cos"       : fmt.Fprintf(w, "%f", cos)
+						case "jaccard"   : fmt.Fprintf(w, "%f", jaccard)
+						case "dice"      : fmt.Fprintf(w, "%f", dice)
+						case "overlap"   : fmt.Fprintf(w, "%f", overlap)
+						case "lift"      : fmt.Fprintf(w, "%f", lift)
+						case "pmi"       : fmt.Fprintf(w, "%f", pmi)
+						case "npmi"      : fmt.Fprintf(w, "%f", npmi)
+						case "anpmi"     : fmt.Fprintf(w, "%f", anpmi)
+						case "tanimoto"  : fmt.Fprintf(w, "%f", tanimoto)
+					}
+					if k==len(output_fmt)-1 {
+						fmt.Fprint(w,"\n")
+					} else {
+						fmt.Fprint(w,"\t")
+					}
+				}
+			}
+			bar.Add(1) // progress // TODO: ilosc intersekcji ???
+		}
+		w.Flush()
+		fo.Close()
+		wg.Done()
+	}
+	
+	W := workers
+	wg.Add(W)
+	for i:=0; i<W; i++ {
+		go f(i,W)
+	}
+	wg.Wait()
+	bar.Close()
+			
+	// press_enter("\npress ENTER to reclaim memory")
+
+	// for m := range users_by_item {
+		// users_by_item[m] = nil
+	// }
+	// users_by_item = nil
+	// runtime.GC()
+	// debug.FreeOSMemory()
+
+	press_enter("\npress ENTER to finish")
+	
+	fmt.Println(items[:10])
+	//fmt.Println(common[:10])
+	
+}
+
+func main() {
+	cfg := Cfg{}
+	
+	flag.StringVar(&cfg.input_path,  "i",   "", "input path")
+	flag.StringVar(&cfg.output_path, "o",   "", "output path template, %d will be replaced with partition number")
+	flag.StringVar(&cfg.output_fmt,  "fmt", "aname,bname,c", "output format")
+	
+	flag.IntVar(&cfg.buf_cap,   "buf",     10, "line buffer capacity in MB")
+	flag.IntVar(&cfg.item_col,  "colitem",  1, "1-based column number of item name")
+	flag.IntVar(&cfg.users_col, "colusers", 2, "1-based column number of users names")
+	flag.IntVar(&cfg.workers,   "wrk",      1, "number of workers")
+	//flag.IntVar(&cfg.workers,   "top",    100, "limit output to top N similarities") // TODO ktora metryka - pierwsza, ostatnia, parametr ???
+	flag.IntVar(&cfg.workers,   "cmin",     1, "minimum number of common users to show in output")
+	
+	flag.Usage = func() {
+		fmt.Printf("Usage of this program:\n")
+		fmt.Printf("./TODO TODO TODO\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	// TODO walidacja cfg
+	core(&cfg)
+}
